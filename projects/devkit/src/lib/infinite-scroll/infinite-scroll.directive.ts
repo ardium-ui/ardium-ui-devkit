712f440f98bfa3da3080138ec3d8ffcb8edf53cc
joker876 <joker876x@gmail.com>
2024-08-31 22:12:50 +0200

    allow infinite scroll directive to listen to document scroll events

--- projects/devkit/src/lib/infinite-scroll/infinite-scroll.directive.ts
+++ projects/devkit/src/lib/infinite-scroll/infinite-scroll.directive.ts
@@ -9,6 +9,13 @@ import {
 } from '@angular/core';
 import { coerceNumberProperty } from './../coercion/number';
 
+export const ArdInfScrollTarget = {
+  Host: 'host',
+  HTML: 'html',
+} as const;
+export type ArdInfScrollTarget =
+  (typeof ArdInfScrollTarget)[keyof typeof ArdInfScrollTarget];
+
 @Directive({
   selector: '[ardInfScroll]',
 })
@@ -20,28 +27,52 @@ export class ArdiumInfiniteScrollDirective {
   private _getElementRect(): DOMRect {
     return this.elementRef.nativeElement.getBoundingClientRect();
   }
-  private _getScrollTop(): number {
+  private _getElementScrollTop(): number {
     return this.elementRef.nativeElement.scrollTop;
   }
-  private _getScrollHeight(): number {
+  private _getElementScrollHeight(): number {
     return this.elementRef.nativeElement.scrollHeight;
   }
 
   //! options
-  readonly ardInfScrollTarget = input<number, any>(200, {
+  readonly ardInfScrollThreshold = input<number, any>(200, {
     transform: (v) => coerceNumberProperty(v, 200),
   });
   readonly ardInfScrollActive = model<boolean>(true, {});
+
+  readonly ardInfScrollTarget = input<ArdInfScrollTarget>(
+    ArdInfScrollTarget.HTML,
+  );
 
   //! event handlers
   @HostListener('scroll')
   onHostScroll() {
     if (!this.ardInfScrollActive()) return;
 
+    if (this.ardInfScrollTarget() !== ArdInfScrollTarget.Host) {
+      console.error(
+        `DKT-NF1000: ardInfScroll directive has target set to "html", but the host element has just been scrolled. To ensure the directive functions properly, make the host element unscrollable.`,
+      );
+    }
     const { height } = this._getElementRect();
-    const scrollTop = this._getScrollTop();
-    const scrollHeight = this._getScrollHeight();
-    if (scrollTop + height > scrollHeight - this.ardInfScrollTarget()) {
+    const scrollTop = this._getElementScrollTop();
+    const scrollHeight = this._getElementScrollHeight();
+    const thresholdOffset = this.ardInfScrollThreshold();
+
+    if (scrollTop + height >= scrollHeight - thresholdOffset) {
+      this.ardInfScrollActive.set(false);
+      this.ardInfScrollReachThreshold.emit();
+    }
+  }
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    if (!this.ardInfScrollActive()) return;
+
+    const { top, height } = this._getElementRect();
+    const viewportHeight = window.innerHeight;
+    const thresholdOffset = this.ardInfScrollThreshold();
+
+    if (-1 * top + viewportHeight >= height - thresholdOffset) {
       this.ardInfScrollActive.set(false);
       this.ardInfScrollReachThreshold.emit();
     }
