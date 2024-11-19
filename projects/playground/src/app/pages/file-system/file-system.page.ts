da68369c57496d8329b8194d1507b5bb3be52ada
joker876 <joker876x@gmail.com>
2024-11-19 21:24:34 +0100

    [playground] fix file system page

--- projects/playground/src/app/pages/file-system/file-system.page.ts
+++ projects/playground/src/app/pages/file-system/file-system.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FileSystemMethod, FileSystemService, FileSystemStartDirectory } from '../../../../../../dist/devkit';
+import { FileSystemMethod, FileSystemService, FileSystemStartDirectory } from '../../../../../devkit/src/public-api';
 
 @Component({
   selector: 'file-system-page',
