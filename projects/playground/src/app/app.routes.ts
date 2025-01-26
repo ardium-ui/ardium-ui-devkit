b878bb14eaa068381d3c42ccf049c4e56387d205
joker876 <joker876x@gmail.com>
2025-01-26 23:34:27 +0100

    [playground] create http service page

--- projects/playground/src/app/app.routes.ts
+++ projects/playground/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FileSystemPage } from './pages/file-system/file-system.page';
 import { HoldPage } from './pages/hold/hold.page';
+import { HttpServicePage } from './pages/http-service/http-service.page';
 import { InfiniteScrollPage } from './pages/infinite-scroll/infinite-scroll.page';
 import { SignalsPage } from './pages/signals/signals.page';
 
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'signals', component: SignalsPage },
   { path: 'file-system', component: FileSystemPage },
   { path: 'hold', component: HoldPage },
+  { path: 'http-service', component: HttpServicePage },
 ];
---
+++ projects/playground/src/app/pages/http-service/http-service.page.ts
@@ -0,0 +1,31 @@
+import { JsonPipe } from '@angular/common';
+import { Component, computed, inject, Injectable, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { createHttpService } from '../../../../../devkit/src/lib/http-service/http.service';
+
+@Injectable({ providedIn: 'root' })
+export class MyHttpService extends createHttpService(
+  'https://jsonplaceholder.typicode.com/',
+) {}
+
+@Component({
+  selector: 'http-service-page',
+  standalone: true,
+  imports: [FormsModule, JsonPipe],
+  providers: [MyHttpService],
+  templateUrl: './http-service.page.html',
+  styleUrl: './http-service.page.scss',
+})
+export class HttpServicePage {
+  private readonly http = inject(MyHttpService);
+
+  readonly id = signal<string>('1');
+
+  readonly users = rxResource({
+    request: () => ({ id: this.id() }),
+    loader: ({ request }) => this.http.get(`/users/${request.id}`),
+  });
+
+  readonly error = computed(() => (this.users.error() as any).message);
+}
