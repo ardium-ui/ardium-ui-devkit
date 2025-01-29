71ae91ae31a2e9825a125dffd48cbd839960cf03
joker876 <joker876x@gmail.com>
2025-01-29 19:04:22 +0100

    [playground] create home page

--- projects/playground/src/app/app.routes.ts
+++ projects/playground/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { FileSystemPage } from './pages/file-system/file-system.page';
 import { HoldPage } from './pages/hold/hold.page';
+import { HomePage } from './pages/home/home.page';
 import { HttpServicePage } from './pages/http-service/http-service.page';
 import { InfiniteScrollPage } from './pages/infinite-scroll/infinite-scroll.page';
 import { SignalsPage } from './pages/signals/signals.page';
 
 export const routes: Routes = [
+  { path: '', component: HomePage },
   { path: 'infinite-scroll', component: InfiniteScrollPage },
   { path: 'signals', component: SignalsPage },
   { path: 'file-system', component: FileSystemPage },
