a07c97cae4ce33a67e7439b86c591c273c85ee96
joker876 <joker876x@gmail.com>
2026-06-16 13:55:15 +0200

    create coerceDateOnlyProperty

--- projects/devkit/src/lib/coercion/date.ts
+++ projects/devkit/src/lib/coercion/date.ts
@@ -30,34 +30,83 @@ export function getUTCDate(
   );
 }
 
-export function coerceDateProperty<T>(
-  v: any,
-  fallback: T,
-  alwaysUTC: boolean = false,
-): Date | T {
+export function coerceDateProperty<T>(v: any, fallback: T): Date | T {
   if (typeof v == 'string') {
     if (v.toLowerCase() === 'now') {
-      if (alwaysUTC) {
-        return getUTCDate();
-      }
       return new Date();
     }
   }
   v = new Date(v);
-  if (alwaysUTC) {
-    v = getUTCDate(
-      v.getFullYear(),
-      v.getMonth(),
-      v.getDate(),
-      v.getHours(),
-      v.getMinutes(),
-      v.getSeconds(),
-      v.getMilliseconds(),
-    );
+  if (isNaN(v.valueOf())) {
+    return fallback;
   }
-  // check if does not contain an error
-  if (!isNaN(v.valueOf())) {
+  return v;
+}
+
+export function coerceDateOnlyProperty<T>(
+  v: any,
+  fallback: T,
+  asUTC: boolean = false,
+): Date | T {
+  if (typeof v == 'string') {
+    if (v.toLowerCase() === 'now' || v.toLowerCase() === 'today') {
+      const now = new Date();
+      now.setMinutes(now.getMinutes() + now.getTimezoneOffset());
+      return asUTC
+        ? getUTCDate(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            now.getHours(),
+            now.getMinutes(),
+            now.getSeconds(),
+            now.getMilliseconds(),
+          )
+        : now;
+    }
+    v = new Date(v);
+    if (isNaN(v.valueOf())) {
+      return fallback;
+    }
     return v;
   }
-  return fallback;
+  const dateV = new Date(v);
+  if (isNaN(dateV.valueOf())) {
+    return fallback;
+  }
+  if (dateV.toISOString().substring(10) === 'T00:00:00.000Z') {
+    if (asUTC) {
+      return dateV;
+    }
+    return new Date(
+      dateV.getUTCFullYear(),
+      dateV.getUTCMonth(),
+      dateV.getUTCDate(),
+      0,
+      0,
+      0,
+      0,
+    );
+  }
+  // hack to detect if the date was created with a time component
+  if (dateV.getMilliseconds() !== 0 || dateV.getSeconds() !== 0 || dateV.getMinutes() % 15 !== 0) {
+    // date has a time component, we treat it as if it was created using Date.now() and thus convert it to a date only value and convert to UTC if needed
+    if (asUTC) {
+      return getUTCDate(
+        dateV.getFullYear(),
+        dateV.getMonth(),
+        dateV.getDate(),
+      );
+    }
+    return new Date(dateV.getFullYear(), dateV.getMonth(), dateV.getDate(), 0, 0, 0, 0);
+  }
+  // we are now quite confident that there is no time component
+  if (asUTC) {
+    return getUTCDate(
+      dateV.getFullYear(),
+      dateV.getMonth(),
+      dateV.getDate(),
+    );
+  }
+  return dateV;
 }
